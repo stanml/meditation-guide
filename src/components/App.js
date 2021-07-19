e1e6aaf9b92e75dd3662fdd8d9a03fe23b001837
Ben Stanley <benstanley@live.co.uk>
2021-07-19 21:45:05 +0100

    hook up mailchimp

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
+      <div>
         <SignUpForm/>
       </div>
     );
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { ReactMailchimpEmailSignupForm } from 'react-mailchimp-email-signup-form';
+import 'react-mailchimp-email-signup-form/dist/esm/index.css';
 
-// Here's a usage example when using functional components
 const SignUpForm = () => {
   return (
     <ReactMailchimpEmailSignupForm
       elementId="first-email-signup-form"
-      url="https://XX.us4.list-manage.com/subscribe/post?u=XXXXXXXXXXXXXX"
-      title="Subscribe to the Newsletter"
+      url={`https://live.us6.list-manage.com/subscribe/post?u=${process.env.REACT_APP_MAILCHIMP_USER}&id=${process.env.REACT_APP_MAILCHIMP_ID}`}
+      title="Sign up to stay updated"
       subtitle="We take privacy seriously and we'll never spam or sell your information."
     />
   );
