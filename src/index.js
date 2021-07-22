92cfdc9807b1d302e23d108dd7b4e3719aee3044
Ben Stanley <benstanley@live.co.uk>
2021-07-22 21:31:29 +0100

    use react router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render((
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
+  ), document.querySelector('#root')
+);
