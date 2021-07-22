59f4cdd0ed7030c5d0e1d77bb3566de1c00e97a3
Ben Stanley <benstanley@live.co.uk>
2021-07-22 22:38:04 +0100

    add content

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import SignUpForm from './SignUpForm';
+import BookTitle from './BookTitle';
+import About from './About'
+import Blurb from './Blurb'
 
 class App extends React.Component {
 
   render() {
     return (
       <div>
+        <BookTitle/>
         <SignUpForm/>
+        <Blurb/>
+        <About/>
       </div>
     );
   }
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -7,8 +7,8 @@ const SignUpForm = () => {
     <ReactMailchimpEmailSignupForm
       elementId="first-email-signup-form"
       url={`https://live.us6.list-manage.com/subscribe/post?u=${process.env.REACT_APP_MAILCHIMP_USER}&id=${process.env.REACT_APP_MAILCHIMP_ID}`}
-      title="Sign up to stay updated"
-      subtitle="We take privacy seriously and we'll never spam or sell your information."
+      title="Sign up here"
+      subtitle="We will let you know when the book is published"
     />
   );
 }
