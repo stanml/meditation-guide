514ccba5624eb366d361717b4682af50751a47c8
Ben Stanley <benstanley@live.co.uk>
2021-07-23 13:40:33 +0100

    more formatting for mobile

--- src/components/BookTitle.js
+++ src/components/BookTitle.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BookTitle = () => {
   return (
-    <div>
+    <div className="ui fluid container">
       <h3>Register your interest for:</h3>
       <h1>Journeys to the Deep: Meditation Guide (volume 1)</h1>
       <p>By Elizabeth English, PhD. Mindfulness Practitioner at Cambridge University.</p>
